35596f8d6a6d88523a4d60a8e3ca255ed077103d
Michael Holub <m.holub@corp.mail.ru>
2017-06-09 23:34:18 +0300

    targeting stubs

---
+++ Assets/Game/Scripts/Targeting/Targeter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeter : MonoBehaviour {
+    public float VisibilityMaxDistance = 10;
+    public float VisibilityAngle = 45.0f;
+
+    public float TargetMaxDistance = 5;
+    public float TargetAngle = 15.0f;
+    
+    void Update()
+    {
+        CheckForTargets();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        var visibilityForward = VisibilityMaxDistance * transform.forward;
+        
+        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, VisibilityAngle / 2, 0) * visibilityForward);
+        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, -VisibilityAngle / 2, 0) * visibilityForward);
+
+        Gizmos.color = Color.red;
+
+        var targetForward = TargetMaxDistance * transform.forward;
+
+        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, TargetAngle / 2, 0) * targetForward);
+        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, -TargetAngle / 2, 0) * targetForward);
+    }
+
+    void CheckForTargets()
+    {
+        foreach (var targetable in Targetable.AllTargets)
+        {
+            var collider = targetable.Collider;
+
+            var direction = (targetable.transform.position - transform.position).normalized;
+            var rotatedDirection = new Vector3(direction.z, 0, -direction.x);
+
+            var midPoint = targetable.transform.position;
+            var leftPoint = midPoint + rotatedDirection * collider.radius;
+            var rightPoint = midPoint - rotatedDirection * collider.radius;
+
+            Debug.DrawLine(transform.position, midPoint);
+            Debug.DrawLine(transform.position, leftPoint);
+            Debug.DrawLine(transform.position, rightPoint);
+        }
+    }
+}

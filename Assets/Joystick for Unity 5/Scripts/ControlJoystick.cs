893e4c76bfc1063fa7d3f77375c1b79b43bd2444
Yaroslav Rastrigin <rastrigin@corp.mail.ru>
2017-06-09 23:41:07 +0300

    HUD

---
+++ Assets/Joystick for Unity 5/Scripts/ControlJoystick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlJoystick : MonoBehaviour {
+
+	public Joystick leftJoystick;
+	public Joystick rightJoystick;
+
+	public float speed = 5f;
+	public float speedRotate = 150f;
+
+	void Update ()
+	{
+		transform.Translate (0f, leftJoystick.virtualAxes.y * Time.deltaTime * speed, 0f);
+		transform.Rotate (0f, 0f, -rightJoystick.virtualAxes.x * Time.deltaTime * speedRotate);
+	}
+}
---
+++ Assets/Joystick for Unity 5/Scripts/ControlPad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPad : MonoBehaviour 
+{
+	public TouchPad leftPad;
+	public TouchPad rightPad;
+	
+	public float speed = 5f;
+	public float speedRotate = 150f;
+	
+	void Update ()
+	{
+		transform.Translate (0f, leftPad.virtualAxes.y * Time.deltaTime * speed, 0f);
+		transform.Rotate (0f, 0f, -rightPad.virtualAxes.x * Time.deltaTime * speedRotate);
+	}	
+
+}

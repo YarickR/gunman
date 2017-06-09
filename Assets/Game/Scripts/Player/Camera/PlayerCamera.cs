20ff3151d4fb6d1052b1d2e4ed6fe87d267a25f6
Michael Holub <m.holub@corp.mail.ru>
2017-06-09 21:17:46 +0300

    simple camera

--- Assets/Game/Scripts/Player/Camera/PlayerCamera.cs
+++ Assets/Game/Scripts/Player/Camera/PlayerCamera.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Cameras;
 
+[ExecuteInEditMode]
 public class PlayerCamera : MonoBehaviour
 {
     public static PlayerCamera instance;
 
     public Camera cam;
-    public AutoCam autoCam;
+    public Transform Target;
+
+    public Vector3 TargetOffset;
 
     private void Awake()
     {
         instance = this;
     }
 
+    public void Update()
+    {
+        if (Target != null)
+        {
+            transform.position = Target.position + TargetOffset;
+        }
+    }
+
     public void SetFollowTransform(Transform targetTransform)
     {
-        autoCam.SetTarget(targetTransform);
+        Target = targetTransform;
     }
 }

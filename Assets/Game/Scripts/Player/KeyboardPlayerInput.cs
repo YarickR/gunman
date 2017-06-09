743a856d6a9878f140687ce4fca7a02f20b4fbda
Michael Holub <m.holub@corp.mail.ru>
2017-06-09 22:35:29 +0300

    keyboard player input

---
+++ Assets/Game/Scripts/Player/KeyboardPlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardPlayerInput : PlayerInput
+{
+    public override Vector2 GetMoveDirection()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    public override Vector2 GetLookDirection()
+    {
+        return new Vector2(Input.mousePosition.x, Input.mousePosition.y) - new Vector2(Screen.width / 2, Screen.height / 2);
+    }
+}
--- Assets/Game/Scripts/Player/PlayerInput.cs
+++ Assets/Game/Scripts/Player/PlayerInput.cs
@@ -1,16 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
-    public Vector2 GetMoveDirection()
-    {
-        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-    }
-
-    public Vector2 GetLookDirection()
-    {
-        return new Vector2(Input.mousePosition.x, Input.mousePosition.y) - new Vector2(Screen.width / 2, Screen.height / 2);
-    }
+    public virtual Vector2 GetMoveDirection() { return Vector2.one; }
+    public virtual Vector2 GetLookDirection() { return Vector2.one; }
 }

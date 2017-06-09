2a0b73fb7a062eab565fd816e321c4ef9a4aa81c
v.sleptsov <v.sleptsov@corp.mail.ru>
2017-06-09 18:49:59 +0300

    добавил немного иерархии проекта

---
+++ Assets/Game/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerCamera cam;
+
+    public float speed;
+}

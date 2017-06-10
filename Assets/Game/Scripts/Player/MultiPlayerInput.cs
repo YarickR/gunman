dc4ca8f854c3176a89032dd75a8f5ad27fb2c73c
Michael Holub <m.holub@corp.mail.ru>
2017-06-10 17:30:03 +0300

    no mouse in mobile build

--- Assets/Game/Scripts/Player/MultiPlayerInput.cs
+++ Assets/Game/Scripts/Player/MultiPlayerInput.cs
@@ -19,6 +19,10 @@ public class MultiPlayerInput : PlayerInput {
         var ld1 = Input1.GetLookDirection();
         if (Input2 == null) return ld1;
         var ld2 = Input2.GetLookDirection();
+        if (Application.isMobilePlatform)
+        {
+            return ld2;
+        }
         if (ld2.sqrMagnitude > 0)
         {
             return ld2;
@@ -34,6 +38,10 @@ public class MultiPlayerInput : PlayerInput {
         var md1 = Input1.GetMoveDirection();
         if (Input2 == null) return md1;
         var md2 = Input2.GetMoveDirection();
+        if (Application.isMobilePlatform)
+        {
+            return md2;
+        }
 
         if (md1.sqrMagnitude > md2.sqrMagnitude)
         {

e5eb898b4babded8c54bdf94ce9e47a095b519e7
Yaroslav Rastrigin <rastrigin@corp.mail.ru>
2017-06-10 15:46:06 +0300

    Muzzle flash

---
+++ Assets/MuzzleFlash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour {
+
+	private float _flashStart;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	public void Flash() {
+		_flashStart = Time.time;
+		gameObject.SetActive(true);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (_flashStart > 0) {
+			if ((Time.time - _flashStart) > 0.1f) { 
+				gameObject.SetActive(false);
+				_flashStart = 0;
+			} 
+		}
+	}
+}

e79ed38b5d0d1e21d9d6e42946d70d87ba2b1471
v.sleptsov <v.sleptsov@corp.mail.ru>
2017-06-10 12:20:18 +0300

    начало логики оружия

---
+++ Assets/Game/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    private Targetable _lastTarget = null;
+
+    private void Update()
+    {
+        //playerController.LineOfSights.
+    }
+}

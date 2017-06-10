7c3a291dd451af068a5f9d82212da7b83b77e4a6
Michael Holub <m.holub@corp.mail.ru>
2017-06-10 21:28:18 +0300

    interactable

---
+++ Assets/Game/Scripts/Interactable/Door.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Door : Interactable
+{
+    public override void Interact(PlayerController player)
+    {
+        Debug.LogFormat("Implement door here");
+    }
+}

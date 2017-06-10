ed2845a91b416962219b18964713bd7d0236b0e4
Michael Holub <m.holub@corp.mail.ru>
2017-06-10 12:19:39 +0300

    game hud in lobby manager

--- Assets/Game/Scripts/Player/JoystickPlayerInput.cs
+++ Assets/Game/Scripts/Player/JoystickPlayerInput.cs
@@ -3,22 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JoystickPlayerInput : PlayerInput {
-	private Joystick __leftJ, __rightJ;
-	public void Start() {
-		if ((GameObject.Find("/HUD/Panel/leftJ") == null) || (GameObject.Find("/HUD/Panel/rightJ") == null)) {
-			Debug.Log("Joystick not found");
-		} else {
-			__leftJ = GameObject.Find("/HUD/Panel/leftJ").GetComponent<Joystick>();
-			__rightJ = GameObject.Find("/HUD/Panel/rightJ").GetComponent<Joystick>() as Joystick;
-		}
-	}
+    public Joystick Left, Right;
+	
     public override Vector2 GetMoveDirection()
     {
-		return new Vector2( __leftJ.virtualAxes.x,  __leftJ.virtualAxes.y );
+        return new Vector2(Left.virtualAxes.x, Left.virtualAxes.y);
     }
 
     public override Vector2 GetLookDirection()
     {
-		return new Vector2(__rightJ.virtualAxes.x, __rightJ.virtualAxes.y);
+        return new Vector2(Right.virtualAxes.x, Right.virtualAxes.y);
     }
 }
